7e0816f1744c078d5ce1098f8f8db85a4444f14c
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2020-07-30 16:30:17 +0500

    add another motivation quotation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-      <h1>It's Better Late than Never!!</h1>
+      <h1>It's Better Late than Never!! So keep trying hard</h1>
   );
 }
 export default App;
